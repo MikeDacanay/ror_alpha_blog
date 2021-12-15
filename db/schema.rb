96ba6c00bc54f79e6a0839c695becc10ce38a49f
Miguel Dacanay <migueldacanay@Miguels-MacBook-Air.local>
2021-12-15 14:48:50 -0800

    commiting before anything

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_030219) do
+ActiveRecord::Schema.define(version: 2021_12_15_042654) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
