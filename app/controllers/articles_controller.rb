1b4986c2a03d71d344aa139a443e8955b94de975
Miguel Dacanay <migueldacanay@Miguels-MacBook-Air.local>
2021-12-16 12:31:00 -0800

    added partials and private methods under controller to DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -11,13 +12,43 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+    end
+
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = 'Article was created successfully!'
             redirect_to @article
         elsif
             render 'new'
         end
+    end
+
+    def update
+        #updates 
+
+        if @article.update(article_params)
+            flash[:notice] = 'Article was edited successfully!'
+            redirect_to @article
+        elsif
+            render 'edit'
+        end
+    end
+
+    def destroy
+        #end
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id]);
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 end
